deef19da9c2c2e4b1c3decab3fa923335cc0f10f
Bruna Grassi <101432594+BrunaGrassi@users.noreply.github.com>
2024-10-02 12:12:17 -0300

    código inicial recebido pelo exercício
    A corrigir:
    
    - Item 1 - Problema relatado referente a reserva de assentos, o programa está aceitando reservar assentos a cima do limite do total de assentos do voo
    - Item 2 - Problema ao cobrar a taxa de embarque, as taxas de embarque são cobradas por meio de valores fixos, porém está sendo cobrado 10% referente ao preço da passagem
    - Item 3 - Problema ao verificar quantidade de assentos disponíveis, esta funcionalidade está retornando o valor errado
    - Item 4 - Não há funcionalidade mostrando o Aeroporto de origem e o Aeroporto de destino em Voo.

---
+++ Passagem.java
@@ -0,0 +1,20 @@
+public class Passagem {
+    private Voo voo;
+    private String nomePassageiro;
+    private double preco;
+
+    public Passagem(Voo voo, String nomePassageiro, double preco) {
+        this.voo = voo;
+        this.nomePassageiro = nomePassageiro;
+        this.preco = preco;
+    }
+
+    public double calcularPrecoTotal() {
+        return preco + preco * 0.10;
+    }
+
+    public String getNomePassageiro() {
+        return nomePassageiro;
+    }
+
+}
---
+++ Voo.java
@@ -0,0 +1,23 @@
+public class Voo {
+    private String codigo;
+    private int totalAssentos;
+    private int assentosOcupados;
+
+    public Voo(String codigo, int totalAssentos) {
+        this.codigo = codigo;
+        this.totalAssentos = totalAssentos;
+        this.assentosOcupados = 0;
+    }
+
+    public void reservarAssento() {
+        assentosOcupados++;
+    }
+
+    public int getAssentosDisponiveis() {
+        return assentosOcupados;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+}
